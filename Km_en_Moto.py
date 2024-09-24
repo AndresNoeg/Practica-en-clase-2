8c70bdad8e52953e532a3db32b6f6b3117de46e4
AndresNoeg <a.23308051281184@cbtis128.edu.mx>
2024-09-23 20:49:18 -0600

    Practica 2 km en moto

---
+++ Km_en_Moto.py
@@ -0,0 +1,14 @@
+print (" ")
+print ("Andres Noe Gomez Garcia Kilometros y litros en moto")
+print(" ")
+
+#Pide a el usuario la cantidad de km
+km=float(input("ingrese la cantidad de km que se recorrieron: "))
+#Explica el gasto promedio de gasolina en un km
+print("En promedio las motos gastan 0.04 litros de gasolina cada km")
+print(" ")
+print("Si su moto recorrio", km , "km los litros de gasolina gastados son: " )
+r=km*0.04
+#Imprime el resultado
+print (r, "litros")
+print(" ")
